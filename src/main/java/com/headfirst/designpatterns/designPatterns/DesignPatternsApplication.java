6dbaf27982bc364e7d5328bbaa7082c3dce7d17c
Lempuris <Lempuris@hotmail.com>
2019-10-20 00:30:42 +0200

    Final strategy commit

--- src/main/java/com/headfirst/designpatterns/designPatterns/DesignPatternsApplication.java
+++ src/main/java/com/headfirst/designpatterns/designPatterns/DesignPatternsApplication.java
@@ -7,18 +7,22 @@ import com.headfirst.designpatterns.designPatterns.strategy.Duck;
 import com.headfirst.designpatterns.designPatterns.strategy.FlyRocketPowered;
 import com.headfirst.designpatterns.designPatterns.strategy.MallardDuck;
 import com.headfirst.designpatterns.designPatterns.strategy.ModelDuck;
+import com.headfirst.designpatterns.designPatterns.strategy.MuteQuack;
 
 @SpringBootApplication
 public class DesignPatternsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DesignPatternsApplication.class, args);
-		
+
 		Duck mallard = new MallardDuck();
 		mallard.performQuack();
 		mallard.performFly();
-		
+
 		Duck model = new ModelDuck();
+		model.performQuack();
+		model.setQuackBehavior(new MuteQuack());
+		model.performQuack();
 		model.performFly();
 		model.setFlyBehavior(new FlyRocketPowered());
 		model.performFly();
