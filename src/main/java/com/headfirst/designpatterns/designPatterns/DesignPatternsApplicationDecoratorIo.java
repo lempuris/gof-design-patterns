9f8747b27e83d76a3e97f6fb440a9c61065ac881
Lempuris <Lempuris@hotmail.com>
2019-10-20 02:36:47 +0200

    InputStream first commit

---
+++ src/main/java/com/headfirst/designpatterns/designPatterns/DesignPatternsApplicationDecoratorIo.java
@@ -0,0 +1,33 @@
+package com.headfirst.designpatterns.designPatterns;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.headfirst.designpatterns.designPatterns.decoratorio.LowerCaseInputStream;
+
+@SpringBootApplication
+public class DesignPatternsApplicationDecoratorIo {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignPatternsApplicationDecoratorIo.class, args);
+
+		int c;
+		try {
+			InputStream in = new LowerCaseInputStream(new BufferedInputStream(new FileInputStream(
+					"C:\\Users\\Admin\\Desktop\\JAVA\\DesignPatterns\\designpatterns\\src\\main\\resources\\text")));
+
+			while ((c = in.read()) > 0) {
+				System.out.print((char) c);
+			}
+
+			in.close();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+	}
+
+}
